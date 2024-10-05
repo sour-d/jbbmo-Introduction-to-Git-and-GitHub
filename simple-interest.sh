341c6f026e502418c116e8a446a5d538888d3bb4
sourav das <36912347+sour-d@users.noreply.github.com>
2024-10-05 23:12:25 +0530

    modified simple interest log

--- simple-interest.sh
+++ simple-interest.sh
@@ -21,6 +21,11 @@ read r
 echo "Enter time period in years:"
 read t
 
+echo "You have entered:"
+echo "principle: ${p}"
+echo "interest: ${r}"
+echo "year: ${t}"
+
 s=$(expr $p \* $t \* $r / 100)
 echo "The simple interest is: "
 echo $s
